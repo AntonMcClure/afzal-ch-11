47df852f64d34e6b6db2710e59dacfc1186cca31
Anton McClure <anton@antonmcclure.com>
2020-07-11 19:03:47 -0400

    initial commit

---
+++ afzal.cpp
@@ -0,0 +1,15 @@
+/* afzal.cpp
+ *
+ * Copyright (C) Anton McClure <asm135@zips.uakron.edu>
+ *
+ * https://github.com/asm135/afzal-ch-11
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hi there!\n";
+	cout << "This is a simple C++ program.\n";
+	return 0;
+}
